d476774c3a100547ab43e267000c60c7755307ed
Sergio Rosa <scarlosjazz@gmail.com>
2022-07-12 20:37:02 -0400

    first commit

---
+++ backend/src/main/java/com/sergiorosa/dsapp/DsappApplication.java
@@ -0,0 +1,13 @@
+package com.sergiorosa.dsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsappApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sergiorosa/dsapp/DsappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergiorosa.dsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
