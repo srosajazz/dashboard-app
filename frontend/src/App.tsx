f34df2e3d6d06f8ab4c1cc326d8da54503590c20
Sergio Rosa <scarlosjazz@gmail.com>
2022-07-14 06:44:47 -0400

    button-icon

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
-
+import NotificationButton from "./components/NotificationButton"
 
 function App() {
+
   return (
+    <>
       <h1>hello world</h1>
- 
+      <NotificationButton />
+      
+    </>
   )
 }
 
